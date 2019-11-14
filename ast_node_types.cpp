8654bac079720b7abd755c89b410a092f5750461
Tinkidinki <mahathi98@gmail.com>
2019-11-15 01:37:26 +0530

    Completed binary operators, error at enclosed expression

--- ast_node_types.cpp
+++ ast_node_types.cpp
@@ -12,16 +12,17 @@ class Operation;
 
 typedef class Node{
     public: 
+        string name;
         virtual void doOp(Operation* op) = 0;
-        virtual string getname() = 0;
+        string getname(){return name;}
 } Node;
 
 class Operation{
     public:
         virtual void LeafOp(Leaf* l) = 0;
         virtual void UnaryOp(Unary* u) = 0;
-        // virtual void BinaryOp(Binary* b) = 0;
-        // virtual void TernaryOp(Ternary* t) = 0;
+        virtual void BinaryOp(Binary* b) = 0;
+        virtual void TernaryOp(Ternary* t) = 0;
         // virtual void ListOp(List* l) = 0;
 };
 
@@ -29,8 +30,8 @@ class PrettyPrint: public Operation{
     public: 
         void LeafOp(Leaf* l);
         void UnaryOp(Unary* u);
-        // void BinaryOp(Binary* b) = 0;
-        // void TernaryOp(Ternary* t) = 0;
+        void BinaryOp(Binary* b);
+        void TernaryOp(Ternary* t);
         // void ListOp(List* l) = 0;
 };
 
@@ -53,10 +54,47 @@ class Unary: public Node{
             op->UnaryOp(this);
         }
         Node* operand;
-
-        virtual string getname() = 0;
 };
 
 void PrettyPrint::UnaryOp(Unary* u){
-    cout << "(" <<  u->getname() << " , " << (u->operand)->getname() <<  ")" << endl;
+    cout << "(" <<  u->getname() << " , \n\t"; 
+    (u->operand)->doOp(this);
+    cout <<  ")\n";
+} 
+
+class Binary: public Node{
+    public:
+        void doOp(Operation* op){
+            op->BinaryOp(this);
+        }
+        Node* left;
+        Node* right;
+};
+
+void PrettyPrint::BinaryOp(Binary* b){
+    cout << "(" <<  b->getname() << " , \n\t "; 
+    (b->left)->doOp(this);
+    cout << ",";
+    (b->right)->doOp(this);
+    cout <<  ")\n";
+} 
+
+class Ternary: public Node{
+    public:
+        void doOp(Operation* op){
+            op->TernaryOp(this);
+        }
+        Node* child1;
+        Node* child2;
+        Node* child3;
+};
+
+void PrettyPrint::TernaryOp(Ternary* t){
+    cout << "(" <<  t->getname() << " , \n\t"; 
+    (t->child1)->doOp(this);
+    cout << ",";
+    (t->child2)->doOp(this);
+    cout << ",";
+    (t->child2)->doOp(this);
+    cout <<  ")\n";
 } 
--- ast_nodes.cpp
+++ ast_nodes.cpp
@@ -6,78 +6,137 @@ using namespace std;
 class IntLiteral: public Leaf{
     public:
         int value;
-        string name = "INT_LIT";
+        
 
         IntLiteral(int val){
+            name = "INT_LIT";
             value = val;
         }
-
-        string getname(){return name;}
         string getvalue(){return to_string(value);}
 };
 
 class CharLiteral: public Leaf{
     public:
         char value;
-        string name = "CHAR_LIT";
 
         CharLiteral(char val){
+            name = "CHAR_LIT";
             value = val;
         }
 
-        string getname(){return name;}
         string getvalue(){return string(1,value);}
 };
 
 class BoolLiteral: public Leaf{
     public:
         bool value;
-        string name = "BOOL_LIT";
-
+        
         BoolLiteral(bool val){
+            name = "BOOL_LIT";
             value = val;
         }
 
-        string getname(){return name;}
         string getvalue(){return string(1,value);}
 };
 
 class StringLiteral: public Leaf{
     public:
         string value;
-        string name = "STRING_LIT";
 
         StringLiteral(string val){
+            name = "STRING_LIT";
             value = val;
         }
 
-        string getname(){return name;}
         string getvalue(){return value;}
 };
 
 class Identifier: public Leaf{
     public:
         string value;
-        string name = "ID";
 
         Identifier(string val){
+            name = "ID";
             value = val;
         }
 
-        string getname(){return name;}
         string getvalue(){return value;}
 };
 
 
 class UnaryExpression: public Unary{
     public:
-        string name;
-        
+     
         UnaryExpression(char* val, Node* expr){
             string str(val);
             name = val;
             operand = expr;
         }
-        string getname(){return name;}
+      
+};
+
+class AssignmentStatement: public Binary{
+    public:
+        
+        AssignmentStatement(Node* loc, Node* expr){
+            name = "ASSIGN";
+            left = loc;
+            right = expr;
+        }
+};
+
+
+class ArithmeticOperator: public Leaf{
+    string value;
+    public:
+        ArithmeticOperator(char* op){
+            name = "OP_ARITH";
+            value = op;
+        }
+        string getvalue(){return value;}
+        
+};
+
+class RelationalOperator: public Leaf{
+    string value;
+    public:
+        RelationalOperator(char* op){
+            name = "OP_REL";
+            value = op;
+        }
+    string getvalue(){return value;}
+        
+};
+
+class EqualOperator: public Leaf{
+    string value;
+    public:
+        EqualOperator(char* op){
+            name = "OP_EQ";
+            value = op;
+        }
+    string getvalue(){return value;}
+        
+};
+
+class ConditionalOperator: public Leaf{
+    string value;
+    public:
+        ConditionalOperator(char* op){
+            name = "OP_COND";
+            value = op;
+        }
+    string getvalue(){return value;}
+        
+};
+
+class BinaryExpression: public Ternary{
+    public:
+        BinaryExpression(Node* expr, Node* op, Node* literal){
+            name = "EXP_BIN";
+            child1 = expr;
+            child2 = op;
+            child3 = literal;
+        }
 };
 
